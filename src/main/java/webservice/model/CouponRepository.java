56ef7d8422b239dc8fb810c4c10faaf5c4a0af12
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:41:17 -0600

    Update CouponRepository.java

--- src/main/java/webservice/model/CouponRepository.java
+++ src/main/java/webservice/model/CouponRepository.java
@@ -2,5 +2,15 @@ package webservice.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CouponRepository extends MongoRepository<Coupon, String> {
+
+    public List<Coupon> findByCodeLikeIgnoreCase(String code);
+
+    public List<Coupon> findByActive(boolean active);
+
+    public Coupon findByCode(String code);
+
+
 }
