2448ebc06d43987c6446f4a0efce8e1b87d3fa88
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:43:11 -0600

    Update HomeController.java

--- src/main/java/webservice/web/HomeController.java
+++ src/main/java/webservice/web/HomeController.java
@@ -1,13 +1,24 @@
 package webservice.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import webservice.model.Coupon;
+import webservice.service.CouponService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private static final String COUPONS_LIST_PAGE = "coupons";
+    private static final String COUPON_ADD_PAGE = "addCoupon";
+
+    @Autowired
+    CouponService couponService;
 
     @RequestMapping("/")
     public String index(@RequestHeader(value="User-Agent", defaultValue="foo") String userAgent,Model model) {
@@ -16,15 +27,17 @@ public class HomeController {
         return "home";
     }
 
-    /*
-     @RequestMapping("/")
-    public String index(@RequestParam(value="name", required=false) String name, Model model) {
-    	if(StringUtils.isNotEmpty(name))
-    	{
-        	model.addAttribute("name", name);
-        }
-        return "home";
+    @RequestMapping("/coupons")
+    public String couponsList(Model model) {
+        List<Coupon> coupons =  couponService.getActiveCoupons(Boolean.TRUE);
+        model.addAttribute("coupons",coupons);
+        return COUPONS_LIST_PAGE;
     }
-    */
+
+    @RequestMapping(value="/coupon", method= RequestMethod.GET)
+    public String couponCreateForm(Model model) {
+        model.addAttribute("coupon", new Coupon());
+        return COUPON_ADD_PAGE;
+    }
 
 }
