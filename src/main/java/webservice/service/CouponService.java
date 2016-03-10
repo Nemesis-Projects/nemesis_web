55298a94c2fa37f302750dfbe6102548fba336bf
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:41:43 -0600

    Update CouponService.java

--- src/main/java/webservice/service/CouponService.java
+++ src/main/java/webservice/service/CouponService.java
@@ -1,8 +1,12 @@
 package webservice.service;
 
 import webservice.model.Coupon;
+
 import java.util.List;
 
 public interface CouponService {
     public List<Coupon> couponsList();
+    public List<Coupon> searchForCouponsByCode (String searchTerm);
+    public List<Coupon> getActiveCoupons( boolean active);
+    public Coupon getCouponForCode(String code);
 }
