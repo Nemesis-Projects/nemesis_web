99fb3d7323bc89373d18006ed7f0e1cfbee89d5e
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:39:29 -0600

    Update CouponsController.java

--- src/main/java/webservice/controllers/CouponsController.java
+++ src/main/java/webservice/controllers/CouponsController.java
@@ -1,28 +1,33 @@
 package webservice.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import webservice.model.Coupon;
 import webservice.service.CouponService;
+
 import java.util.List;
-import javax.inject.Inject;
+
 
 @RestController
 public class CouponsController {
 
-    private final CouponService couponService;
-    private final AtomicLong counter = new AtomicLong();
-    
-    
-    @RequestMapping("/coupons")
+
+
+    @Autowired
+    CouponService couponService;
+
+    //@RequestMapping(method= RequestMethod.GET)
+    @RequestMapping("/allcoupons")
     public List<Coupon> couponsList() {
         return couponService.couponsList();
     }
 
-    @Inject
-    public CouponsController(final CouponService couponService) {
-        this.couponService = couponService;
+    @RequestMapping("/coupons/search")
+    public List<Coupon> searchForCoupons(@RequestParam(value="code", defaultValue="A") String couponCode) {
+        return couponService.searchForCouponsByCode(couponCode);
     }
+
+
 }
