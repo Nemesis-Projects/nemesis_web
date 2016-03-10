fa7edd1bb8523754ad728fac760cd7e7992025a9
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:47:15 -0600

    Update HomeController.java

--- src/main/java/webservice/web/HomeController.java
+++ src/main/java/webservice/web/HomeController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import webservice.model.Coupon;
 import webservice.service.CouponService;
+import org.springframework.boot.CommandLineRunner;
+import java.util.*;
 
-import java.util.List;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommandLineRunner {
 
     private static final String COUPONS_LIST_PAGE = "coupons";
     private static final String COUPON_ADD_PAGE = "addCoupon";
@@ -38,6 +39,58 @@ public class HomeController {
     public String couponCreateForm(Model model) {
         model.addAttribute("coupon", new Coupon());
         return COUPON_ADD_PAGE;
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+
+        createEssentialDataCoupons();
+
+    }
+
+    private void createEssentialDataCoupons()
+    {
+        if(couponRepository.count() == 0 ) {
+            List<Coupon> coupons = couponRepository.save(createCoupons());
+            System.out.println("Succesfully created " + coupons.size() + " coupons ");
+        }
+
+    }
+
+    private List<Coupon> createCoupons()
+    {
+        List<Coupon> coupons = new ArrayList<Coupon>();
+        for (int i = 0 ; i < 10 ; i++)
+        {
+            Calendar cal = new GregorianCalendar();
+            Coupon coupon = new Coupon();
+            coupon.setCode(UUID.randomUUID().toString());
+            if( i%3 == 0) {
+                coupon.setActive(Boolean.FALSE);
+                coupon.setRedemptions(Long.valueOf(100 + 10*i));
+                int year = 52 - i*5;
+                cal.add(Calendar.WEEK_OF_YEAR, -year);
+                coupon.setStartDate(cal.getTime());
+                cal.add(Calendar.WEEK_OF_YEAR, -year + 10);
+                coupon.setEndDate(cal.getTime());
+                coupon.setEnabled(Boolean.FALSE);
+            }
+            else
+            {
+                int year = 52 - i*5;
+                cal.add(Calendar.WEEK_OF_YEAR, -year);
+                coupon.setStartDate(cal.getTime());
+                cal.add(Calendar.WEEK_OF_YEAR, +year);
+                coupon.setEndDate(cal.getTime());
+                coupon.setActive(Boolean.TRUE);
+                coupon.setRedemptions(Long.valueOf(0));
+                coupon.setEnabled(Boolean.TRUE);
+            }
+            coupon.setMaxRedemptions(Long.valueOf(1000 + 100 * i));
+            coupons.add(coupon);
+        }
+
+        return coupons;
     }
 
 }
