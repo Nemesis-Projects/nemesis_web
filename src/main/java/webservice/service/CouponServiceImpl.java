ed6e7082dcf89b0a44979729897e773bafa2226b
Neelgagan <neelgagan.adepu@gmail.com>
2016-03-10 15:42:16 -0600

    Update CouponServiceImpl.java

--- src/main/java/webservice/service/CouponServiceImpl.java
+++ src/main/java/webservice/service/CouponServiceImpl.java
@@ -1,52 +1,46 @@
 package webservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import webservice.model.Coupon;
 import webservice.model.CouponRepository;
-import java.util.List;
+
 import java.util.ArrayList;
-import org.springframework.stereotype.Service;
-import javax.inject.Inject;
+import java.util.List;
 
 @Service
 public class CouponServiceImpl implements CouponService {
 
-    private final CouponRepository couponRepository;
-    private final AtomicLong counter = new AtomicLong();
-    
+    @Autowired
+    CouponRepository couponRepository;
+
+    @Override
+    public Coupon getCouponForCode(String code) {
+        Assert.notNull(code);
+        return couponRepository.findByCode(code);
+    }
+
     @Override
     public List<Coupon> couponsList() {
-        Coupon coupon = new Coupon();
-        final String nameCounter = "CNXT-" + counter.incrementAndGet();
-        coupon.setCode(nameCounter);
-        if(isDBError(coupon))
-        {
-            return new ArrayList<Coupon>();
-        }
-        //not persistent entity error when find something
         List<Coupon> couponList = couponRepository.findAll();
-        return couponList;
+        return couponList != null ? couponList : new ArrayList<Coupon>();
     }
 
-    private boolean isDBError(final Coupon coupon)
+
+
+    @Override
+    public List<Coupon> searchForCouponsByCode (final String searchTerm)
     {
-        try
-        {
-            couponRepository.save(coupon);
-            return Boolean.FALSE;
-        } catch (Exception e)
-        {
-
-        }
-        return Boolean.TRUE;
+        Assert.notNull(searchTerm);
+        return couponRepository.findByCodeLikeIgnoreCase(searchTerm);
     }
 
-    @Inject
-    public CouponServiceImpl(final CouponRepository couponRepository) {
-        this.couponRepository = couponRepository;
+    @Override
+    public List<Coupon> getActiveCoupons(boolean active) {
+        List<Coupon> couponList = couponRepository.findByActive(Boolean.TRUE);
+        return couponList != null ? couponList : new ArrayList<Coupon>();
     }
+
+
 }
